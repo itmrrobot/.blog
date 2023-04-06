790d6c96e3df5029ab7e944ca31c20ed57c7e9e2
itmrrobot <nhannguyentri05@gmail.com>
2023-04-06 20:05:19 +0700

    Push to github

---
+++ frontend/src/constants/index.js
@@ -0,0 +1,4 @@
+export const ACCOUNTURL = "http://localhost:4000/user/me";
+export const ACCOUNTPASSWORDURL = "http://localhost:4000/user/password";
+export const USERBLOGSURL = "http://localhost:4000/blogs/me";
+export const PATHBLOGSURL = "http://localhost:4000/blogs";
